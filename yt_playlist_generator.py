52acad0583154fe2e366df1ab8dcb57d08a4d105
Daniel Berlyne <danberlyne@gmail.com>
2023-08-14 08:17:25 +0100

    added `if __name__ == '__main__'`

--- yt_playlist_generator.py
+++ yt_playlist_generator.py
@@ -1,9 +1,13 @@
 #!/usr/bin/env python3
 # yt_playlist_generator.py - Creates a YouTube playlist from a list of URLs.
 
-file = open('urls.txt')
-urls = [line.strip() for line in file.readlines()]
-file.close()
-ids = [url[-11:] for url in urls]
-playlist = 'https://www.youtube.com/watch_videos?video_ids=' + ','.join(ids)
-print(f'Playlist created at: {playlist}')
+def main():
+    file = open('urls.txt')
+    urls = [line.strip() for line in file.readlines()]
+    file.close()
+    ids = [url[-11:] for url in urls]
+    playlist = 'https://www.youtube.com/watch_videos?video_ids=' + ','.join(ids)
+    print(f'Playlist created at: {playlist}')
+
+if __name__ == '__main__':
+    main()
